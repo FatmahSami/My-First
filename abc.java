65cda634350198af336fa03dc2c10c87ad8ae2a6
FatmahSami <47919470+FatmahSami@users.noreply.github.com>
2019-02-26 21:41:25 +0300

    My First

---
+++ abc.java
@@ -0,0 +1,13 @@
+package abc;
+
+public class abc{
+
+public static void main(String args[]){
+
+String a = "a";
+String b = "b";
+String c = "c";
+System.out.println("The first three letters of the alphabet are: " + a + " " + b + " " + c);
+}
+
+}
